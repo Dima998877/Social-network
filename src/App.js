ba7fa81b699c05f00a167d418692b2d35fdf85a0
Dima Karkanitsa <d.karkanitsa@gmail.com>
2022-12-19 21:59:52 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import wallpaper from './wallpaper.jpg'
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <nav className='App-nav'>
+        <div><a>Profile</a></div>
+        <div><a>Messages</a></div>
+        <div><a>News</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
+
+      </nav>
+      <div className='App-content'>
+        <img src={wallpaper} className='App-wallpaper' alt='wallpaper' />
+        <div>icon+discrip</div>
+        <div>
+          <div>My post</div>
+          <div>New post</div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+   return  <header className='App-header'>
+   <img src='https://www.svgrepo.com/show/426053/git.svg' className='App-logo' alt='logo' />
+ </header>;
+}
+
+export default Header;
