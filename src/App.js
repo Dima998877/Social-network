38968aa78f4bb94b13ad14b073aa2a63d483cd05
Dima Karkanitsa <d.karkanitsa@gmail.com>
2022-12-26 17:50:09 +0100

    Added 3 jsx files

--- src/App.js
+++ src/App.js
@@ -2,30 +2,17 @@
 import './App.css';
 import wallpaper from './wallpaper.jpg'
 import Header from './components/Header';
+import Nav from './components/Navbar';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <nav className='App-nav'>
-        <div><a>Profile</a></div>
-        <div><a>Messages</a></div>
-        <div><a>News</a></div>
-        <div><a>Music</a></div>
-        <div><a>Settings</a></div>
-
-      </nav>
-      <div className='App-content'>
-        <img src={wallpaper} className='App-wallpaper' alt='wallpaper' />
-        <div>icon+discrip</div>
-        <div>
-          <div>My post</div>
-          <div>New post</div>
-          <div>Post 1</div>
-          <div>Post 2</div>
-        </div>
-      </div>
+      <Navbar />
+      <Profile />
     </div>
   );
 }
