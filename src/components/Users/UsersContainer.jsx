67f7b33be36ab2ca3c93c239d5ecc83f11120cae
Dima Karkanitsa <d.karkanitsa@gmail.com>
2023-02-07 17:22:28 +0100

    Preloader is added

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,36 +1,44 @@
 import React from "react";
 import { connect } from "react-redux";
-import { followAC, setCurrentPageAC, setTotalUserCountAC, setUsersAC, unfollowAC } from "../../redux/users-reducer";
-import Axios from'axios'
+import { followAC, setCurrentPageAC, setTotalUserCountAC, setUsersAC, unfollowAC, setIsFetchingAC } from "../../redux/users-reducer";
+import Axios from 'axios'
 import Users from './Users'
+import preloader from '../../assets/images/Preloader.gif'
 
 
 class UsersContainer extends React.Component {
    componentDidMount() {
+      this.props.setIsFetching(true)
       Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
          .then((response) => {
             this.props.setUsers(response.data.items)
             this.props.setTotalUsersCount(response.data.totalCount)
+            this.props.setIsFetching(false)
          })
    }
    onPageChanged = (pageNumber) => {
       this.props.setCurrentPage(pageNumber)
+      this.props.setIsFetching(true)
       Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-      .then((response) => {
-         this.props.setUsers(response.data.items)
-      })}
+         .then((response) => {
+            this.props.setIsFetching(false)
+            this.props.setUsers(response.data.items)
+         })
+   }
 
-   render () {
-      return <Users 
-      totalUsersCount={this.props.totalUsersCount}
-      pageSize={this.props.pageSize}
-      onPageChanged={this.onPageChanged}
-      currentPage={this.props.currentPage}
-      users={this.props.users}
-      unfollow={this.props.unfollow}
-      follow={this.props.follow}
-      /> 
-      
+   render() {
+      return <>
+         {this.props.isFetching ? <img src={preloader} alt='' /> : 
+         <Users
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            onPageChanged={this.onPageChanged}
+            currentPage={this.props.currentPage}
+            users={this.props.users}
+            unfollow={this.props.unfollow}
+            follow={this.props.follow}
+         />}
+      </>
    }
 
 }
@@ -40,7 +48,8 @@ let mapStateToProps = (state) => {
       users: state.usersPage.users,
       totalUsersCount: state.usersPage.totalUsersCount,
       pageSize: state.usersPage.pageSize,
-      currentPage: state.usersPage.currentPage
+      currentPage: state.usersPage.currentPage,
+      isFetching: state.usersPage.isFetching
    }
 }
 let mapDispatchToProps = (dispatch) => {
@@ -48,8 +57,9 @@ let mapDispatchToProps = (dispatch) => {
       follow: (userId) => { dispatch(followAC(userId)) },
       unfollow: (userId) => { dispatch(unfollowAC(userId)) },
       setUsers: (users) => { dispatch(setUsersAC(users)) },
-      setCurrentPage: (currentPage) => {dispatch(setCurrentPageAC(currentPage))},
-      setTotalUsersCount: (totalCount) => {dispatch(setTotalUserCountAC(totalCount))}
+      setCurrentPage: (currentPage) => { dispatch(setCurrentPageAC(currentPage)) },
+      setTotalUsersCount: (totalCount) => { dispatch(setTotalUserCountAC(totalCount)) },
+      setIsFetching: (currentState) => { dispatch(setIsFetchingAC(currentState)) }
    }
 
 }
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,12 +4,14 @@ const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
 const SET_TOTAL_USER_COUNT= 'SET_TOTAL_USER_COUNT'
+const SET_IS_FETCHING= 'SET_IS_FETCHING'
 
 const initialState = {
       users: [ ],
       pageSize: 10,
       totalUsersCount: 1,
       currentPage: 1,
+      isFetching: false
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -40,6 +42,8 @@ const usersReducer = (state = initialState, action) => {
          return {...state, currentPage: action.currentPage}
       case SET_TOTAL_USER_COUNT:
          return {...state, totalUsersCount: action.totalCount}
+      case SET_IS_FETCHING:
+         return {...state, isFetching: action.currentState}
       default:
          return state;
    }
@@ -49,4 +53,5 @@ export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
 export const setUsersAC = (users) => ({ type: SET_USERS, users })
 export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage })
 export const setTotalUserCountAC = (totalCount) => ({ type: SET_TOTAL_USER_COUNT, totalCount })
+export const setIsFetchingAC = (currentState) => ({ type: SET_IS_FETCHING, currentState })
 export default usersReducer
