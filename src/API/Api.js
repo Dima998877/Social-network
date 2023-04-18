20fb66925a72eaba215c9488e64ddbeddc99bac9
Dima Karkanitsa <d.karkanitsa@gmail.com>
2023-04-18 22:57:35 +0200

    env added

--- src/API/Api.js
+++ src/API/Api.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 const instance = Axios.create({
    withCredentials: true,
-   headers: { 'API-KEY': '0c3ac84b-f72f-426a-89b3-3c5bf477590c' },
+   headers: process.env.REACT_APP_API_KEY,
    baseURL: 'https://social-network.samuraijs.com/api/1.0/'
 })
 export const usersAPI = {
@@ -17,29 +17,7 @@ export const usersAPI = {
       return instance.post(`follow/${userId}`).then(response => response.data)
    },
 } 
-// export const profileAPI = {
-//    getProfile(userId) {
-//       return instance.get(`profile/${userId}`).then(response => response.data)
-//    },
-//    getProfileStatus(userId){
-//       return instance.get(`profile/status/${userId}`).then(response => response.data)
-//    },
-//    updateProfileStatus(status){
-//       return instance.put(`profile/status/`, {status: status}).then(response => response.data)     
-//    },
-//    updatePhoto(photoFile){
-//       let formData = new FormData();
-//       formData.append('image', photoFile)
-//       return instance.put('/profile/photo/', formData, {
-//          headers: {
-//             'Content-Type': 'multipart/form-data'
-//          }
-//        }).then(response => response.data)
-//    },
-//    saveProfileData(profile){
-//       return instance.put(`profile`, profile).then(response => response.data)     
-//    }
-// } 
+
 export const securityAPI = {
    getCaptcha() {
       return instance.get(`security/get-captcha-url`).then(response => response.data)
